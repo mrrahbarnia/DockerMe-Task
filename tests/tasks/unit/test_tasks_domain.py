d6618637e63e3554dd7cc867dfeff888415d6d51
Mohammadreza Rahbarnia <mrrahbarnia@gmail.com>
2026-01-05 11:50:52 +0330

    Added more tests.

--- tests/tasks/unit/test_tasks_domain.py
+++ tests/tasks/unit/test_tasks_domain.py
@@ -25,4 +25,7 @@ async def test_send_taskran_event_successfully():
         created_at=datetime.now(),
     )
     task.run()
+
     assert any(isinstance(event, events.TaskRan) for event in task.events)
+    assert len(task.events) == 1
+    assert task.status == types.TaskStatusEnum.RUNNING
